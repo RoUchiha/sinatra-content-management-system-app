7992b36d42c454c6e16ad3b0b74aee8ec1614247
RoUchiha <swaggsikh@gmail.com>
2021-11-24 17:34:45 -0600

    layout.erb created.

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,6 +1,16 @@
 class TrainersController < ApplicationController
 
+    get '/signup' do
+        if !logged_in?
+            erb :'trainers/new'
+        else
+            redirect "/trainers"
+        end
+    end
 
+    post 'signup' do
+        
+    end
 
 
 

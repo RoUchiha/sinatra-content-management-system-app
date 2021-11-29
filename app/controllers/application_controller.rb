fc859aeadbe60e1b35591e9dfb79fb247eb57432
RoUchiha <swaggsikh@gmail.com>
2021-11-29 13:42:49 -0600

    login view and get/post done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,12 @@ class ApplicationController < Sinatra::Base
     end
 
     get '/' do
-        erb :index
+        if logged_in?
+            @trainer = Trainer.find_by_id(session[:user_id])
+            redirect "/trainers/#{@trainer.slug}"
+        else
+            erb :index
+        end
     end
 
     helpers do
--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -6,11 +6,18 @@ class TrainersController < ApplicationController
         erb :'trainers/show'
     end
     
-
+    get '/trainers/:slug/edit' do
+        @trainer = Trainer.find_by_slug(params[:slug])
+        if logged_in? && session[:user_id] == @trainer.id
+            erb :'trainers/index'
+        else 
+            redirect "/trainers/:slug"
+        end
+    end
 
     get '/signup' do
         if !logged_in?
-            erb :'trainers/new', locals: {message: "Please create an account before signing in"}
+            erb :'trainers/new', locals: {message: "Please create an account before signing in."}
         else
             redirect "/trainers"
         end
@@ -18,7 +25,7 @@ class TrainersController < ApplicationController
 
     post '/signup' do
         if params[:trainer_name] == "" || params[:gender] == "" || params[:email] == "" || params[:password] == ""
-            redirect '/signup'
+            erb :'trainers/new', locals: {message: "Please fill out all fields!"}
         else
             @trainer = Trainer.new(:trainer_name => params[:trainer_name], :gender => params[:gender], :email => params[:email], :password => params[:password])
             @trainer.save
@@ -27,6 +34,33 @@ class TrainersController < ApplicationController
         end
     end
 
+    get '/login' do
+        if !logged_in?
+            erb :'trainers/login'
+        else
+            @trainer = Trainer.find_by_id(session[:user_id])
+            redirect "/trainers/#{@trainer.slug}"
+        end
+    end
 
+    post '/login' do
+        @trainer = Trainer.find_by(:email => params[:email])
+
+        if @trainer && @trainer.authenticate(params[:password])
+            session[:user_id] = @trainer.id
+            redirect "/trainers/#{@trainer.slug}"
+        else
+            erb :'trainers/login', locals: {message: "Email and/or password is incorrect! Please check credentials and try again."}
+        end
+    end
+
+    get '/logout' do
+        if session[:user_id] != nil
+            session.destroy
+            redirect '/'
+        else
+            redirect '/'
+        end
+    end
 
 end

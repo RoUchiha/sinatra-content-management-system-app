c1be88cc26bbb39c07efa38e6b70704e4ee2cb17
RoUchiha <swaggsikh@gmail.com>
2021-11-29 19:18:09 -0600

    pokemon show view done.

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -24,11 +24,13 @@ class PokemonsController < ApplicationController
         if params[:name] == "" || params[:type1] == ""
             erb :'pokemons/new', locals: {message: "Please fill out at least Name and Type!"}
         else
-            @pokemon = current_user.pokemons.build(:name => params[:name], :nickname => params[:nickname], :type1 => params[:typr1], :type2 => params[:type2])
-            @pokemon.save
-            if params[:best_buddy] == 1
+            @trainer = Trainer.find(session[:user_id])
+            @pokemon = Pokemon.new(name: params[:name], nickname: params[:nickname], type1: params[:type1], type2: params[:type2], trainer_id: @trainer.id)
+            if params[:best_buddy] != nil
                 @pokemon.best_buddy = true
             end
+            @pokemon.save
+            
             redirect "/pokemons/#{@pokemon.slug}"
         end
     end
